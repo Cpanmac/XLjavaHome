286453405d8bae0c708bf5c3fd0f6b5a5ad75d38
xuli <78645574@qq.com>
2017-09-19 13:39:09 +0800

    增加CalendarDemo

---
+++ src/com/xl/calendar/CalendarDemo.java
@@ -0,0 +1,26 @@
+package com.xl.calendar;
+
+import org.junit.Test;
+
+import java.util.Calendar;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ÐìÁ¢
+ * Date: 2017/9/19
+ * Time: 13:29
+ * To change this template use File | Settings | File Templates.
+ */
+public class CalendarDemo {
+    /**
+     * monthÊÇ-1µÄ
+     */
+    @Test
+    public void calendarTest() {
+        Calendar c = Calendar.getInstance();
+        System.out.println("YEAR:"+c.get(Calendar.YEAR));
+        System.out.println("MONTH:"+c.get(Calendar.MONTH));
+        System.out.println("DAY_OF_YEAR:"+c.get(Calendar.DAY_OF_YEAR));
+        System.out.println("DAY_OF_MONTH:"+c.get(Calendar.DAY_OF_MONTH));
+    }
+}
