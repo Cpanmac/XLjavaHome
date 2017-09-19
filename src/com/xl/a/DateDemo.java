b0b842802a07c3cf92f85074bb586f2f95a8e75b
xuli <78645574@qq.com>
2017-09-19 14:45:34 +0800

    修改datetDemo

--- src/com/xl/a/DateDemo.java
+++ src/com/xl/a/DateDemo.java
@@ -8,35 +8,9 @@ import java.util.Date;
 
 public class DateDemo {
     @Test
-    public void test2() {
-        Date now = new Date();
-        SimpleDateFormat sf = new SimpleDateFormat("yyyyMMddhhmm");
-        // 年份2+月份2+日期2+毫秒3
-        // sf = new SimpleDateFormat("yyMMddsS");
-        sf = new SimpleDateFormat("yyMMdd");
-        String s = sf.format(now) + "000001";
-        System.out.println(s);
-        System.out.println(s.length());
-        long i = Long.parseLong(s);
-        System.out.println(i);
-        s = Long.parseLong(s) + 1 + "";
-        System.out.println(s);
-        System.out.println(s.length());
-    }
-
-    @Test
-    public void test1() {
-        for (int i = 0; i <= 10; i++) {
-            // test2();
-            test();
-        }
-    }
-
-    @Test
     public void test() {
-        Calendar c = Calendar.getInstance();
-        System.out.println(c.getTimeInMillis());
-        System.out.println(Long.toString(System.currentTimeMillis()).substring(1).length());
-        System.out.println(Long.toString(System.currentTimeMillis()).substring(1));
+        Date d = new Date();
+        System.out.println(d.getMonth());
+        System.out.println(d.getDay());
     }
 }
