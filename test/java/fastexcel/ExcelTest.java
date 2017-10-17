31ccf6299fd0b28f94e946af47e436a6c5143025
xuli <78645574@qq.com>
2017-10-17 17:05:35 +0800

    excel

--- test/java/fastexcel/ExcelTest.java
+++ test/java/fastexcel/ExcelTest.java
@@ -1,6 +1,7 @@
 package fastexcel;
 
 import com.github.sd4324530.fastexcel.FastExcel;
+import com.xl.util.FileTool;
 import fastexcel.entity.MyTest;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.junit.Test;
@@ -8,34 +9,30 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 /**
- * @author peiyu
+ * Created with IntelliJ IDEA.
+ * User: 徐立
+ * Date: 2017-10-17
+ * Time: 13:56
+ * To change this template use File | Settings | File Templates.
  */
 public class ExcelTest {
     private static final Logger LOG = LoggerFactory.getLogger(ExcelTest.class);
+    InputStream is = FileTool.getResourceInputStream("data.xlsx");
 
     @Test
-    public void test() {
-        try {
-            createExcel();
-        } catch (IOException e) {
-            LOG.error("异常", e);
-        } catch (InvalidFormatException e) {
-            LOG.error("异常", e);
-        }
-    }
-
-    private void createExcel() throws IOException, InvalidFormatException {
-        FastExcel fastExcel = new FastExcel("E:/data.xlsx");
+    public void test() throws IOException, InvalidFormatException {
+        FastExcel fastExcel = new FastExcel(is);
         fastExcel.setSheetName("活动信息数据");
         List<MyTest> tests = fastExcel.parse(MyTest.class);
         if (null != tests && !tests.isEmpty()) {
             for (MyTest myTest : tests) {
                 LOG.debug("记录:{}", myTest.toString());
             }
-            FastExcel create = new FastExcel("E:/data2.xlsx");
+            FastExcel create = new FastExcel(FileTool.getResourceFile("excel.xls").getAbsolutePath());
             create.setSheetName("活动信息数据");
             boolean result = create.createExcel(tests);
             LOG.debug("结果:{}", result);
