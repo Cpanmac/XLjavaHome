4dc6d73bb9590911331ad57bf2ab2343c27df665
xuli <78645574@qq.com>
2017-10-21 10:15:55 +0800

    添加定时任务

---
+++ src/main/java/src/com/xl/entity/xutils/utils/ValidatorsTest.java
@@ -0,0 +1,19 @@
+package com.xl.entity.xutils.utils;
+
+import com.xuan.xutils.utils.Validators;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by xuan on 17/6/7.
+ */
+public class ValidatorsTest {
+    @Test
+    public void testIsBlank() {
+        Assert.assertTrue(Validators.isBlank(null));
+        Assert.assertTrue(Validators.isBlank(""));
+        Assert.assertTrue(Validators.isBlank(" "));
+        Assert.assertTrue(!Validators.isBlank("bob"));
+        Assert.assertTrue(!Validators.isBlank(" bob "));
+    }
+}
---
+++ src/main/java/src/javaWeb/com/xl/CodingFilter.java
@@ -0,0 +1,34 @@
+package javaWeb.com.xl;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URLDecoder;
+
+public class CodingFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        servletRequest.setCharacterEncoding("UTF-8");
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        String lastUri = request.getRequestURI();
+        String path = URLDecoder.decode(lastUri, "utf-8");
+        request.getRequestDispatcher(path).forward(request, response);
+        //servletResponse.setCharacterEncoding("utf-8");
+        //filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
