67d1067f0ed0df2702c0849338ca58f27214e1c7
xuli <78645574@qq.com>
2017-10-30 17:26:29 +0800

    修改POM.xml

---
+++ src/main/java/com/xl/XLMain.java
@@ -0,0 +1,14 @@
+package com.xl;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: 徐立
+ * Date: 2017-10-30
+ * Time: 17:16
+ * To change this template use File | Settings | File Templates.
+ */
+public class XLMain {
+    public static void main(String args) {
+        System.out.println("这是主函数");
+    }
+}
