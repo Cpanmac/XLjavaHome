bbcc70da656960ddbbd59b17a99f65d3d07629a5
xuli <78645574@qq.com>
2017-11-21 15:13:01 +0800

    init

--- src/test/java/com/xl/properties/SystemInfoTest.java
+++ src/test/java/com/xl/properties/SystemInfoTest.java
@@ -1,19 +1,29 @@
 package com.xl.properties;
 
+import com.xl.util.FileTool;
 import com.xl.util.Print;
+import org.junit.After;
 import org.junit.Test;
 
-import java.io.FileNotFoundException;
+import java.io.File;
+import java.io.IOException;
 import java.io.PrintStream;
 import java.util.Properties;
 
 public class SystemInfoTest {
     private Properties pro = System.getProperties(); // 系
+    private File file;
 
     @Test
-    public void demoTest() throws FileNotFoundException {
+    public void demoTest() throws IOException {
         Print.info(pro);
         //在项目目录下生成
-        pro.list(new PrintStream("生成一个txt.txt"));
+        file = FileTool.createResourceFile("生成一个txt.txt");
+        pro.list(new PrintStream(file));
+    }
+
+    @After
+    public void after() throws IOException {
+        FileTool.open(file);
     }
 }
--- src/test/java/com/xl/quartz/QuartzTest.java
+++ src/test/java/com/xl/quartz/QuartzTest.java
@@ -14,26 +14,6 @@ import java.util.Date;
  * To change this template use File | Settings | File Templates.
  */
 public class QuartzTest {
-    @Test
-    public void demoTest() throws InterruptedException {
-        String job_name = "动态任务调度";
-        Print.info("【任务启动】开始(每10秒输出一次)...");
-        QuartzManager.addJob(job_name, QuartzJob.class, "0/10 * * * * ?");
-        Thread.sleep(5000);
-        Print.info("【修改时间】开始(每2秒输出一次)...");
-        QuartzManager.modifyJobTime(job_name, "10/2 * * * * ?");
-        Thread.sleep(6000);
-        Print.info("【移除定时】开始...");
-        QuartzManager.removeJob(job_name);
-        Print.info("【移除定时】成功");
-        Print.info("【再次添加定时任务】开始(每10秒输出一次)...");
-        QuartzManager.addJob(job_name, QuartzJob.class, "*/10 * * * * ?");
-        Thread.sleep(60000);
-        Print.info("【移除定时】开始...");
-        QuartzManager.removeJob(job_name);
-        Print.info("【移除定时】成功");
-    }
-
     public static String formatDateByPattern(Date date, String dateFormat) {
         SimpleDateFormat sdf = new SimpleDateFormat(dateFormat);
         String formatTimeStr = null;
@@ -43,6 +23,11 @@ public class QuartzTest {
         return formatTimeStr;
     }
 
+    public static String getCron(java.util.Date date) {
+        String dateFormat = "ss mm HH dd MM ? yyyy";
+        return formatDateByPattern(date, dateFormat);
+    }
+
     @Test
     public void demoTest() throws InterruptedException {
         String job_name = "动态任务调度";
@@ -63,8 +48,23 @@ public class QuartzTest {
         Print.info("【移除定时】成功");
     }
 
-    public static String getCron(java.util.Date date) {
-        String dateFormat = "ss mm HH dd MM ? yyyy";
-        return formatDateByPattern(date, dateFormat);
+    @Test
+    public void demoTest2() throws InterruptedException {
+        String job_name = "动态任务调度";
+        Print.info("【任务启动】开始(每10秒输出一次)...");
+        QuartzManager.addJob(job_name, QuartzJob.class, "0/10 * * * * ?");
+        Thread.sleep(5000);
+        Print.info("【修改时间】开始(每2秒输出一次)...");
+        QuartzManager.modifyJobTime(job_name, "10/2 * * * * ?");
+        Thread.sleep(6000);
+        Print.info("【移除定时】开始...");
+        QuartzManager.removeJob(job_name);
+        Print.info("【移除定时】成功");
+        Print.info("【再次添加定时任务】开始(每10秒输出一次)...");
+        QuartzManager.addJob(job_name, QuartzJob.class, "*/10 * * * * ?");
+        Thread.sleep(60000);
+        Print.info("【移除定时】开始...");
+        QuartzManager.removeJob(job_name);
+        Print.info("【移除定时】成功");
     }
 }
